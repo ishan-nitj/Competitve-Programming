f69db024974fe6a8ae3cad777f767a915c214b25
ishan-nitj <ishan.nitj@gmail.com>
2016-06-06 17:27:49 +0530

    kasai algorithm for building lcp from suffix array in linear time

---
+++ Strings/kasai alg for building lcp from sa.cpp
@@ -0,0 +1,45 @@
+//Ishan Bansal
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mag 100000
+#define inf 1000000000000000000
+#define MOD 1000000007
+#define rep(i,n) for(i=0;i<n;i++)
+#define mset(x,v) memset(x, v, sizeof(x))
+#define print_array(a,n) for(i=0;i<n;i++) cout<<a[i]<<" ";
+#define var_val(x) cout<<#x<<" "<<x<<endl;
+#define pb push_back
+#define fe first
+#define se second
+
+vector<int> kasai(string s, vector<int> sa)
+{
+    int n=s.size(),k=0;
+    vector<int> lcp(n,0);
+    vector<int> rank(n,0);
+
+    for(int i=0; i<n; i++) rank[sa[i]]=i;
+	s
+    for(int i=0; i<n; i++, k?k--:0)
+    {
+        if(rank[i]==n-1) {k=0; continue;}
+        int j=sa[rank[i]+1];
+        while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
+        lcp[rank[i]]=k;
+    }
+    return lcp;
+}
+
+int main()
+{
+ll i;
+string s="banana";
+vector<int>sa={5,3,1,0,4,2};
+vector<int>lcp=kasai(s,sa);
+print_array(lcp,lcp.size());
+}
+
+/* Bazinga */
+
+
